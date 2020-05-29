2a5217ead0e96cc1c81adf2e283bb256705d57f1
trysd <itrysd@icloud.com>
2020-05-30 03:09:04 +0900

    added lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Sample1Module } from 'src/app/sample1/sample1.module';
+import { Sample2Module } from 'src/app/sample2/sample2.module';
+
+import { HeaderComponent } from 'src/app/container/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    Sample1Module,
+    Sample2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
